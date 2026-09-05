7efcc7659708c035bd171da0c361cc10e0af3fcd
EspenAlbert <albertespen@gmail.com>
2026-09-05 15:07:27 +0100

    chore: regen

--- ask_shell/__init__.py
+++ ask_shell/__init__.py
@@ -3,16 +3,16 @@
 from ask_shell import shell
 from ask_shell import console
 from ask_shell import shell_events
+from ask_shell import ask
 from ask_shell.settings import AskShellSettings
 from ask_shell.settings import ShellRunSummary
-from ask_shell import ask
 
 VERSION = "0.11.1"
 __all__ = [
     "shell",
     "console",
     "shell_events",
+    "ask",
     "AskShellSettings",
     "ShellRunSummary",
-    "ask",
 ]
